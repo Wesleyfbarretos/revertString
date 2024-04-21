d5e49633f34b11ef5d663d614fe73b1a0c56bb39
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-04-21 14:53:11 -0300

    feat: testing2

--- revert-string.go
+++ revert-string.go
@@ -1,10 +1,13 @@
 package revertString
 
+import "fmt"
+
 func Revert(s string) string {
 	b := []byte(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
 
+	fmt.Println("alo")
 	return string(b)
 }
